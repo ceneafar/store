1f85eaf8160acd5ce63d34c5122e90f282ccf521
ceneafar <carteaga460@gmail.com>
2023-05-11 21:48:10 -0400

    new font added

--- view/layouts/header.php
+++ view/layouts/header.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="view/css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu&subset=cyrillic,latin' rel='stylesheet' type='text/css' />
 </head>
 <body>
