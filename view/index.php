ee944d2a36b5bbee92a4e5391ca8c92606803014
ceneafar <carteaga460@gmail.com>
2023-05-11 21:43:26 -0400

    style of the forms added

--- view/index.php
+++ view/index.php
@@ -3,10 +3,10 @@ require_once("layouts/header.php");
 ?>
 
 <form method="post" class="form">
-    <input type="text" name="username" placeholder="username">
-    <input type="text" name="password" placeholder="password">
-    <input type="submit" name="flag" value="login">
-    <a href="index.php?flag=signup">sign up</a>
+    <input class="input" type="text" name="username" placeholder="username">
+    <input class="input" type="text" name="password" placeholder="password">
+    <input class="buttons" type="submit" name="flag" value="login">
+    <a class="buttons" href="index.php?flag=signup">sign up</a>
 </form>
 
 <?php
--- view/signup.php
+++ view/signup.php
@@ -3,11 +3,11 @@ require_once("layouts/header.php");
 ?>
 
 <form action="./index.php" method="post" class="form">
-    <input type="text" name="username" placeholder="username">
-    <input type="text" name="password" placeholder="password">
-    <input type="text" name="password2" placeholder="repeat password">
-    <input type="submit" name="flag" value="create">
-    <a href="index.php">back</a>
+    <input class="input" type="text" name="username" placeholder="username">
+    <input class="input" type="text" name="password" placeholder="password">
+    <input class="input" type="text" name="password2" placeholder="repeat password">
+    <input class="buttons" type="submit" name="flag" value="create">
+    <a class="buttons" href="index.php">back</a>
 </form>
 
 <?php
