73ea56df69ee767afe27d6ea2f0f60dc29494ba5
ceneafar <carteaga460@gmail.com>
2023-05-11 21:30:04 -0400

    styles added to the pages

--- view/index.php
+++ view/index.php
@@ -2,13 +2,12 @@
 require_once("layouts/header.php");
 ?>
 
-<form method="post">
+<form method="post" class="form">
     <input type="text" name="username" placeholder="username">
     <input type="text" name="password" placeholder="password">
     <input type="submit" name="flag" value="login">
+    <a href="index.php?flag=signup">sign up</a>
 </form>
-
-<a href="index.php?flag=signup">sign up</a>
 
 <?php
 require_once("layouts/footer.php");
--- view/signup.php
+++ view/signup.php
@@ -2,14 +2,13 @@
 require_once("layouts/header.php");
 ?>
 
-<form action="./index.php" method="post">
+<form action="./index.php" method="post" class="form">
     <input type="text" name="username" placeholder="username">
     <input type="text" name="password" placeholder="password">
     <input type="text" name="password2" placeholder="repeat password">
     <input type="submit" name="flag" value="create">
+    <a href="index.php">back</a>
 </form>
-
-<a href="index.php">back</a>
 
 <?php
 require_once("layouts/footer.php");
