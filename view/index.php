d69ac28b682f2980ae87ec46b57553ec3ee1fc06
ceneafar <carteaga460@gmail.com>
2023-05-23 08:38:43 -0400

    show password in login

--- view/index.php
+++ view/index.php
@@ -5,7 +5,8 @@ require_once("layouts/header.php");
 <form method="post" class="form">
     <h1>log in</h1>
     <input class="input" type="text" name="username" placeholder="username">
-    <input class="input" type="password" name="password" placeholder="password">
+    <input id='pass' class="input" type="password" name="password" placeholder="password">
+    <label for="checkbox1"><input type="checkbox" id="checkbox1" onclick="showPass()">show</label>
     <?php
     if (isset($_COOKIE["message"])) {
         echo "<span>" . $_COOKIE["message"] . "</span>";
@@ -15,6 +16,13 @@ require_once("layouts/header.php");
     <input class="buttons" type="submit" name="flag" value="login">
     <a class="buttons" href="index.php?flag=signup">sign up</a>
 </form>
+
+<script>
+    function showPass() {
+        let element = document.getElementById('pass');
+        element.type == "password" ? element.type = "text" : element.type = "password";
+    }
+</script>
 
 <?php
 require_once("layouts/footer.php");
