c16921822bf5d9ba3cb4e9a042e551936021e03a
ceneafar <carteaga460@gmail.com>
2023-05-23 20:55:14 -0400

    users can see both passwords on the signup form

--- view/signup.php
+++ view/signup.php
@@ -5,8 +5,9 @@ require_once("layouts/header.php");
 <form action="./index.php" method="post" class="form">
     <h1>create account</h1>
     <input class="input" type="text" name="username" placeholder="username">
-    <input class="input" type="password" name="password" placeholder="password">
-    <input class="input" type="password" name="password2" placeholder="repeat password">
+    <input class="input" id='pass1' type="password" name="password" placeholder="password">
+    <input class="input" id='pass2' type="password" name="password2" placeholder="repeat password">
+    <label for="checkbox1"><input type="checkbox" id="checkbox1" onclick="showPass1()">show</label>
     <?php
     if (isset($_COOKIE["message"])) {
         echo "<span>" . $_COOKIE["message"] . "</span>";
@@ -16,6 +17,16 @@ require_once("layouts/header.php");
     <input class="buttons" type="submit" name="flag" value="create">
     <a class="buttons" href="index.php">back</a>
 </form>
+
+<script>
+    function showPass1() {
+        let element = document.getElementById('pass1');
+        let element2 = document.getElementById('pass2');
+
+        element.type == "password" ? element.type = "text" : element.type = "password";
+        element2.type == "password" ? element2.type = "text" : element2.type = "password";
+    }
+</script>
 
 <?php
 require_once("layouts/footer.php");
