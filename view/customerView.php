5923a6d147581b38b7b663cfe1f1fc61b4b77245
ceneafar <carteaga460@gmail.com>
2023-05-29 17:42:31 -0400

    values for customer form added

--- view/customerView.php
+++ view/customerView.php
@@ -15,8 +15,24 @@ class CustomerView
     public function createCustomer()
     {
         echo "<form id='createCustomer' action='index.php?customer=createCustomer' method='post'>";
-        echo "<input type='text' name='name'>";
+
+        echo "<label for='nameCustomer'>name</label>";
+        echo "<input id='nameCustomer' type='text' name='name' placeholder='name'>";
+
+        echo "<label for='lastNameCustomer'>last name</label>";
+        echo "<input id='lastNameCustomer' type='text' name='lastName' placeholder='last name'>";
+
+        echo "<label for='addressCustomer'>address</label>";
+        echo "<input id='addressCustomer' type='text' name='address' placeholder='address'>";
+
+        echo "<label for='phoneNumberCustomer'>phone numner</label>";
+        echo "<input id='phoneNumberCustomer' type='text' name='phoneNumber' placeholder='phone number'>";
+
+        echo "<label for='emailCustomer' for='email'>email</label>";
+        echo "<input id='emailCustomer' type='text' name='email' placeholder='email'>";
+
         echo "<input type='submit' value='create customer'>";
+
         echo "</form>";
     }
 }
