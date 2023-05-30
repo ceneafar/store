069f3c50bd4fbe37a9e4bf7c0be1d228a4c47b03
ceneafar <carteaga460@gmail.com>
2023-05-30 08:16:49 -0400

    more fields added to the product form

--- view/productView.php
+++ view/productView.php
@@ -3,7 +3,7 @@
 class ProductView
 {
     public function showProducts($productsList)
-    {        
+    {
         echo "<ul id='product' class='content'>";
         echo "<h2>Products</h2>";
         foreach ($productsList as $product) {
@@ -15,8 +15,27 @@ class ProductView
     public function createProduct()
     {
         echo "<form id='createProduct' action='index.php?product=createProduct' method='post'>";
-        echo "<input type='text' name='name'>";
+
+        echo "<label for='productName'>product name</label>";
+        echo "<input id='productName' type='text' name='name' placeholder='product name'>";
+
+        echo "<label for='productBrand'>brand</label>";
+        echo "<input id='productBrand' type='text' name='productBrand' placeholder='brand'>";
+
+        echo "<label for='measurementUnit'>measurement unit</label>";
+        echo "<input id='measurementUnit' type='text' name='measurementUnit' placeholder='measurement unit'>";
+
+        echo "<label for='measurementValue'>measurement value</label>";
+        echo "<input id='measurementValue' type='text' name='measurementValue' placeholder='measurement value'>";
+
+        echo "<label for='propertyType'>property type</label>";
+        echo "<input id='propertyType' type='text' name='propertyType' placeholder='property type'>";
+
+        echo "<label for='propertyValue'>property value</label>";
+        echo "<input id='propertyValue' type='text' name='propertyValue' placeholder='property value'>";
+
         echo "<input type='submit' value='create product'>";
+
         echo "</form>";
     }
 }
