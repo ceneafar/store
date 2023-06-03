49d4b98121d28e34fdd0ecd0d9a9bb6332b22e23
ceneafar <carteaga460@gmail.com>
2023-06-02 22:58:14 -0400

    supplier form added

--- view/supplierView.php
+++ view/supplierView.php
@@ -6,6 +6,17 @@ class SupplierView
         echo "
         <div id='supplier'>
             <h2>Suppliers</h2>
+            <form>
+                <div>
+                    <label for='supplierName'>Supplier name</label>
+                    <input id='supplierName' type='text' placeholder='Supplier name'>
+                </div>
+                <div>
+                    <label for='identityCard'>Identity card</label>
+                    <input id='identityCard' type='text' placeholder='Identity Card'>
+                </div>
+                <input type='submit' value='Save'>
+            </form>            
         </div>
         ";
     }
